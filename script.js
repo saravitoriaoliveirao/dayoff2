a9f8dbf06d8b3ca9237d658aae93c0f019302be5
Rickzx7 <ricardoaraujocandido516@gmail.com>
2025-03-13 11:55:19 -0300

    modo claro incompleto 75%

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-const button =
-    document.getElementById("troca-tema");
-    const body = document.body;
+const button = document.getElementById("troca-tema");
 
-
-    button.onclick = () => {
-        body.classList.toggle("modo-escuro");
-        if 
-        (body.classList.contains("modo-escuro")) {
-            localStorage.setItem("tema" , "escuro");
-
-            
-        } else {
-            localStorage.setItem("tema", "claro");
-        }
+button.addEventListener("click", () => {
+    if (document.body.classList.contains("modo-escuro")) {
+        document.body.classList.remove("modo-escuro");
+        document.body.classList.add("modo-claro");
+        localStorage.setItem("tema", "claro");
+    } else {
+        document.body.classList.remove("modo-claro");
+        document.body.classList.add("modo-escuro");
+        localStorage.setItem("tema", "escuro");
     }
-    
+});
+
+// Aplicar a preferência do usuário ao carregar a página
+window.onload = () => {
+    const savedTheme = localStorage.getItem("tema");
+    if (savedTheme === "escuro") {
+        document.body.classList.add("modo-escuro");
+    } else {
+        document.body.classList.add("modo-claro");
+    }
+};
